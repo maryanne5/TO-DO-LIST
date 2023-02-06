15f4fcc641891e163dc8bf7cd0bda284277bc452
maryanne5 <71869462+maryanne5@users.noreply.github.com>
2023-02-06 21:03:37 +0200

    Delete mary directory

--- mary/src/components/Loading/Loading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Loading = () => {
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '200px' }}>
-            <img src='/assets/loading.gif' width='43' height='43' alt='loading icon' />
-        </div>
-    )
-}
